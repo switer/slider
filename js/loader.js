39ef3c64dfdb160f478a5d326b91946824d82f57
guanks <guankaishe@gmail.com>
2013-04-02 15:10:54 +0800

    add bowser

--- js/loader.js
+++ js/loader.js
@@ -1,5 +1,15 @@
 define('loader', ['../web.fs.mobile/src/webfs'], function (wfs) {
-	window.altbox.start();
-    Core.startAll();
-
+	document.getElementById('appContainer').className = 'dp-none';
+	if (!bowser.chrome) {
+		var body = document.body;
+		body.style.backgroundColor = 'black';
+		body.style.overflow = 'hidden';
+		var mask = document.getElementById('mask');
+		mask.className = '';
+		mask.style.height =  document.documentElement.clientHeight + 'px';
+	} else {
+		$('#appContainer').removeClass('dp-none');
+		window.altbox.start();
+	    Core.startAll();
+	}
 });
