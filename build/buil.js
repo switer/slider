8d872eb24222448344a330d611b2de0cf562e091
guanks <guankaishe@gmail.com>
2013-04-10 15:23:07 +0800

    clean and minify buildir

--- build/buil.js
+++ build/buil.js
@@ -2,7 +2,8 @@
 	此脚本用于构建主页引用文件数据包sourceMap
 **/
 var fs 		= require('fs'),
-	cmMinify = require('./min-build.js'),
+	UglifyJS = require("uglify-js"),
+	cssmin = require('ycssmin').cssmin,
 	file 	= 'index.html',
 	cmpJS 	= '../sourceMap.js',
 	source  =  {
@@ -10,7 +11,7 @@ var fs 		= require('fs'),
 		'header' 		: 'header.html',
 		'blogHeader' 	: 'blog/blogHeader.html',
 		'blogFooter' 	: 'blog/blogFooter.html',
-		'cmThemeJS' 	: 'codemirror/cm.theme.js',
+		'cmThemeJS' 	: 'codemirror/codeMirrorThemes.js',
 		'cmThemeCSS' 	: 'codemirror/codeMirrorThemes.css',
 		'cmJS' 			: 'codemirror/codeMirrorJS.js',
 		'cmCSS' 		: 'codemirror/codeMirrorCSS.css',
@@ -22,13 +23,22 @@ var fs 		= require('fs'),
 		'impressHeader' : 'impress/impressHeader.html',
 		'impressFooter' : 'impress/impressFooter.html',
 	}
-console.log('minify codemirror themes');
-cmMinify.min();
 var ctn,
 	sourceMap = {};
 console.log('create sourceMap object');
 for (var name in source) {
-	ctn = fs.readFileSync(source[name], 'UTF-8');
+	if (source[name].match(/.*\.js$/) && !source[name].match(/.*\.min\.js$/)) {
+		console.log('$JS minify -> ' + name);
+		ctn = UglifyJS.minify(source[name]).code;
+	}
+	else if (source[name].match(/.*\.css$/)) {
+		console.log('#CSS minify -> ' + name);
+		ctn = cssmin(source[name]);
+	}
+	else {
+		console.log('-common -> ' + name);
+		ctn = fs.readFileSync(source[name], 'UTF-8');
+	}
 	sourceMap[name] = ctn;
 }
 console.log('write sourceMap file');
--- build/min-build.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs'),
-	UglifyJS = require("uglify-js");
-
-exports.min = function () {
-	var result = UglifyJS.minify("codemirror/codeMirrorThemes.js");
-	fs.writeFileSync('codemirror/cm.theme.js', result.code, 'utf-8');
-}
